897c0c6a093a8f062c438a702f6f1f0dcba6dd76
Mean Sareth <sarethmean086@gmail.com>
2017-12-26 23:18:00 -0800

    initial commit

---
+++ logo.php
@@ -0,0 +1,257 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+    <meta name="description" content="Sabay News proudly is the most visited and popular Khmer website, with more than 20 million page views and almost 2 millions unique visitors per month!" />
+    <meta name="keywords" content="breaking news, entertainment, technology, life, sport, cambodia" />
+    <title>Sabay News</title>
+    <link rel="SHORTCUT ICON" href="http://news.sabay.com.kh/favicon.ico" type="image/x-icon">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width,initial-scale=1" name=viewport>
+    <!-- facebook meta--> 
+    <meta property="og:image" content="http://media.sabay.com/media/publications/logos/sabay-news.png" />    
+    <meta property="og:url" content="http://news.sabay.com.kh" />
+    <meta property="og:title" content="Sabay News" />
+    <meta property="og:description" content="Sabay News proudly is the most visited and popular Khmer website, with more than 20 million page views and almost 2 millions unique visitors per month!" />
+    <meta property="fb:pages" content="1056231387765931" />
+    <meta property="fb:app_id" content="745303362249043"/>
+    <meta property="fb:admins" content="100001452918892"/>
+    <!-- twitter meta-->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@sabayweb">
+    <meta name="twitter:creator" content="@sabayweb">
+    <meta name="twitter:title" content="Sabay News">
+    <meta name="twitter:description" content="Sabay News proudly is the most visited and popular Khmer website, with more than 20 million page views and almost 2 millions unique visitors per month!">
+    <meta name="twitter:image" content="http://media.sabay.com/media/publications/logos/sabay-news.png">
+    <link href="http://news.sabay.com.kh/build/css/app-47db106224.css" rel="stylesheet" type="text/css" />
+  <script type="text/javascript" >
+      var DOMAIN = "http://news.sabay.com.kh";
+  </script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+      </head>
+
+  <body>
+<section id="top">
+    <div class="container">
+      <div class="logo">
+        <a href="http://news.sabay.com.kh">
+          <img src="logo.png">     
+        </a>
+      </div>
+      <div class="ads web ads_items" id="ad_zone_01"></div>
+    </div>
+	</section>
+  <div id="before_menu"></div>
+	<!-- Navigation -->
+  <nav id="menu" class="navbar navbar-inverse navbar-static-top " role="navigation">
+    <div class="container">
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="http://news.sabay.com.kh"><img src="http://news.sabay.com.kh/img/logo.png"></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active home">
+                    <a href="http://news.sabay.com.kh"><i class="fa fa-home"></i></a>
+                </li>
+                                                                                  <li class="category " id="entertainment">
+                    <a href="http://news.sabay.com.kh/topics/entertainment" class="menu_entertainment">
+                        កម្សាន្ត
+                    </a>
+                </li>
+                                                                                    <li class="category " id="technology">
+                    <a href="http://news.sabay.com.kh/topics/technology" class="menu_technology">
+                        បច្ចេកវិទ្យា
+                    </a>
+                </li>
+                                                                                    <li class="category " id="life">
+                    <a href="http://news.sabay.com.kh/topics/life" class="menu_life">
+                        ជីវិតនិងសង្គម
+                    </a>
+                </li>
+                                                                      <style type="text/css">#menu.navbar-inverse .navbar-nav > li > a.menu_sport:hover {color: #04304C;}#menu.navbar-inverse .navbar-nav > li.active > a.menu_sport {background: #04304C;}#menu.navbar-inverse .navbar-nav > li.active > a.menu_sport:hover {color: #ffffff;cursor: default;}</style>
+                                            <li class="category " id="sport">
+                    <a href="http://news.sabay.com.kh/topics/sport" class="menu_sport">
+                        កីឡា
+                    </a>
+                </li>
+                                                                                    <li class="link">
+                  <a href="http://kanha.sabay.com.kh" class="web" target="_blank">
+                    កញ្ញា
+                  </a>
+                  <a href="http://kanha.sabay.com.kh" target="_blank" class="mobile menu_kanha">
+                        <img src="http://media.sabay.com/media/publications/logos/kanha_97x72_extra_small.png" />
+                    </a>
+                </li>
+                                                                                    <li class="link">
+                  <a href="http://enovel.sabay.com.kh" class="web" target="_blank">
+                    ប្រលោមលោក
+                  </a>
+                  <a href="http://enovel.sabay.com.kh" target="_blank" class="mobile menu_enovel">
+                        <img src="http://media.sabay.com/media/sabay-news/logos/Enovelextrasmall_extra_small.png" />
+                    </a>
+                </li>
+                                                                                    <li class="link">
+                  <a href="http://der.sabay.com.kh" class="web" target="_blank">
+                    សប្បាយដើរ
+                  </a>
+                  <a href="http://der.sabay.com.kh" target="_blank" class="mobile menu_sabay_der">
+                        <img src="http://media.sabay.com/media/sabay-news/logos/sabayder-logoupdate9090extrasmall_extra_small.png" />
+                    </a>
+                </li>
+                                                                                                  <li class="tag " id="wedding-tips">
+                    <a href="http://news.sabay.com.kh/article/tag/wedding-tips" class="web menu_wedding-tips">
+                      <img src="http://media.sabay.com/media/Commercial/Micro-Site/WeddingPlan/microButton_B_97x72px.png" alt="" />
+
+                    </a>
+                    <a href="http://news.sabay.com.kh/article/tag/wedding-tips" class="mobile menu_wedding-tips">
+                        <img src="http://media.sabay.com/media/Commercial/Micro-Site/WeddingPlan/microButton_B_97x72px_extra_small.png" />
+                    </a>
+                </li>
+                                                                                    <li class="tag " id="phnom-penh-city-center">
+                    <a href="http://news.sabay.com.kh/article/tag/phnom-penh-city-center" class="web menu_phnom-penh-city-center">
+                      <img src="http://media.sabay.com/media/Commercial/Micro-Site/PPCC/PPCC_microButton_97x72px.png" alt="" />
+
+                    </a>
+                    <a href="http://news.sabay.com.kh/article/tag/phnom-penh-city-center" class="mobile menu_phnom-penh-city-center">
+                        <img src="http://media.sabay.com/media/Commercial/Micro-Site/PPCC/PPCC_microButton_97x72px_extra_small.png" />
+                    </a>
+                </li>
+                                                                                    <li class="tag " id="road-home-2">
+                    <a href="http://news.sabay.com.kh/article/tag/road-home-2" class="web menu_road-home-2">
+                      <img src="http://media.sabay.com/media/Commercial/Micro-Site/TheRoadHome2/microButton_97x72px.png" alt="" />
+
+                    </a>
+                    <a href="http://news.sabay.com.kh/article/tag/road-home-2" class="mobile menu_road-home-2">
+                        <img src="http://media.sabay.com/media/Commercial/Micro-Site/TheRoadHome2/microButton_97x72px_extra_small.png" />
+                    </a>
+                </li>
+                                                                                    <li class="tag " id="tiger-street-football-festival-2018">
+                    <a href="http://news.sabay.com.kh/article/tag/tiger-street-football-festival-2018" class="web menu_tiger-street-football-festival-2018">
+                      <img src="http://media.sabay.com/media/Commercial/Micro-Site/TigerStreet2018/microButton_B_97x72px.png" alt="" />
+
+                    </a>
+                    <a href="http://news.sabay.com.kh/article/tag/tiger-street-football-festival-2018" class="mobile menu_tiger-street-football-festival-2018">
+                        <img src="http://media.sabay.com/media/Commercial/Micro-Site/TigerStreet2018/microButton_B_97x72px_extra_small.png" />
+                    </a>
+                </li>
+                                                                                    <li class="tag " id="weekly-deals">
+                    <a href="http://news.sabay.com.kh/article/tag/weekly-deals" class="web menu_weekly-deals">
+                      <img src="http://media.sabay.com/media/Commercial/Micro-Site/WeeklyDeals/microButton_97x72px.png" alt="" />
+
+                    </a>
+                    <a href="http://news.sabay.com.kh/article/tag/weekly-deals" class="mobile menu_weekly-deals">
+                        <img src="http://media.sabay.com/media/Commercial/Micro-Site/WeeklyDeals/microButton_97x72px_extra_small.png" />
+                    </a>
+                </li>
+                                                                                    <li class="tag " id="short-films-challenge-2017">
+                    <a href="http://news.sabay.com.kh/article/tag/short-films-challenge-2017" class="web menu_short-films-challenge-2017">
+                      <img src="http://media.sabay.com/media/Commercial/Micro-Site/Soyo/microButton_.png" alt="" />
+
+                    </a>
+                    <a href="http://news.sabay.com.kh/article/tag/short-films-challenge-2017" class="mobile menu_short-films-challenge-2017">
+                        <img src="http://media.sabay.com/media/Commercial/Micro-Site/Soyo/microButton__extra_small.png" />
+                    </a>
+                </li>
+                                          <li class="more dropdown"> 
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-ellipsis-h"></i></a>
+                  <ul id="overflow" class="dropdown-menu dropdown-menu-right">
+                  </ul>
+                </li>
+            </ul>
+            <div class="category"><img class="sabay-menu-logo img-responsive" src="http://news.sabay.com.kh/img/footer-logo.png"></div>
+        </div>
+        <!-- /.navbar-collapse -->
+                    </div>
+    <!-- /.container -->
+  </nav>    
+<section id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 copyright">
+				<img class="footer-logo" src="http://news.sabay.com.kh/img/footer-logo.png">
+				<p class="small">​© រក្សា​សិទ្ធិ​គ្រប់​យ៉ាង​ដោយ​ Sabay ឆ្នាំ​២០១៦</p>
+				<p class="small">
+					<strong>អាសយដ្ឋាន</strong> <br />
+					អគារ​លេខ ៣០៨ មហាវិថីព្រះមុន្នីវង្ស <br /> 
+					សង្កាត់បឹងរាំង ខណ្ឌដូនពេញ
+				</p>
+			</div>
+
+			<div class="col-md-5 about">
+				<div class="title">អំពីយើង</div>
+				<p>
+					<strong>Sabay Digital Corporation</strong> ជា​ក្រុមហ៊ុន​ព័ត៌មាន​ឌីជីថល និង​កម្សាន្ត​ឈាន​មុខ​គេ​នៅ​កម្ពុជា។
+					<a href="http://sabay.com/">ព័ត៌មាន​បន្ថែម</a>
+				</p>
+				
+				<p>
+					 <strong> ផលិត​ផល​ និង​ សេវាកម្ម របស់ Sabay </strong>
+				</p>
+				<div class="clear"></div>
+				<ul>
+														<li>
+						<a href="http://news.sabay.com.kh" target="_blank">
+							<img src="http://media.sabay.com/media/publications/logos/sabay_news_logo_extra_small_extra_small.jpg" />
+						</a>
+					</li>
+														<li>
+						<a href="http://kanha.sabay.com.kh" target="_blank">
+							<img src="http://media.sabay.com/media/publications/logos/kanha_extra_small_extra_small.png" />
+						</a>
+					</li>
+														<li>
+						<a href="http://enovel.sabay.com.kh" target="_blank">
+							<img src="http://media.sabay.com/media/publications/logos/enovel-app-icon_extra_small_extra_small.png" />
+						</a>
+					</li>
+														<li>
+						<a href="http://kleykley.sabay.com.kh" target="_blank">
+							<img src="http://media.sabay.com/media/publications/logos/kley_kley_logo_extra_small.png" />
+						</a>
+					</li>
+														<li>
+						<a href="http://der.sabay.com.kh" target="_blank">
+							<img src="http://media.sabay.com/media/publications/logos/sabay_der_logo_extra_small.png" />
+						</a>
+					</li>
+								</ul>
+				<div class="clear"></div>
+			</div>
+
+			<div class="col-md-3 socail copyright">
+				<div class="title">ជួបគ្នានៅបណ្តាញសង្គម</div>
+				<div class="list-socail">
+					<a href="https://www.facebook.com/SabayNewsOfficial/"><i class="fa fa-facebook"></i></a>
+					<a href="https://www.instagram.com/sabaydigital/"><i class="fa fa-instagram"></i></a>
+					<a href="https://www.youtube.com/user/SabayTv"><i class="fa fa-youtube"></i></a>
+				</div>
+
+				<p class="small" style="margin-top:15px;">
+					<strong>ទំនាក់ទំនង</strong> <br />
+					<a href="mailto:info@sabay.com">Info@sabay.com</a> <br />
+					023 228 000
+				</p>
+
+			</div>
+		</div>
+
+		<!-- <div class="ads web start_up_ads" id="ad_zone_05" style="display: none;"></div> -->
+
+	</div>
+</section>
+                    
+    </body>
+</html>
